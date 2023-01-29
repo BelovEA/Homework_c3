a9c6a9f8af6d7dc9450832495c89ba9eccfa1463
BelovEA <vrachea676@gmail.com>
2023-01-29 19:51:45 +0300

    make 1st ex

---
+++ ex_19(1)/ex_19(1)hw3.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом
+//14212 -> нет 12821 -> да 23432 -> да
+int n;
+while(!int.TryParse(Console.ReadLine(), out n))
+{
+    Console.Write("Введен неверный параметр, введите целое пятизначное число: ");
+}
+int Palindrome(int n)
+{
+int first_number = n / 10000;
+int second_number = (n / 1000) % 10;
+int third_number = (n / 100) % 10;
+int fourth_number = (n / 10) % 10;
+int fifth_number = (n % 10);
+    if (first_number == fifth_number && second_number == fourth_number)
+    
+        return 1;
+    
+    else
+    
+        return 2;
+}
+
+var result = Palindrome(n);
+
+if (result == 1)
+{
+    Console.WriteLine("да");
+}
+if (result == 2)
+{
+    Console.WriteLine("нет");
+}
